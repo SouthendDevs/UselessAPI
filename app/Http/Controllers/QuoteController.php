a7d6067c4ff1a97d7f8b09148c01a1e72b97def7
mikeaag <mike.gatward@xenonsoft.co.uk>
2016-03-15 18:34:44 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\File;
 use League\Csv\Reader;
 
 class QuoteController extends Controller
@@ -20,24 +17,21 @@ class QuoteController extends Controller
      */
     public function index(Request $request)
     {
-
         return Response::json([
-            'quote' => self::QUOTE_CHAR_L . 
-                       htmlentities($request->input('text'), ENT_QUOTES, 'UTF-8', false) . 
-                       self::QUOTE_CHAR_R . 
-                       self::DASH_CHAR . 
-                       $this->randomAttribution()]);
-
+            'quote' => self::QUOTE_CHAR_L.
+                       htmlentities($request->input('text'), ENT_QUOTES, 'UTF-8', false).
+                       self::QUOTE_CHAR_R.
+                       self::DASH_CHAR.
+                       $this->randomAttribution(), ]);
     }
 
     /**
-     * @return String
+     * @return string
      */
     public function randomAttribution()
     {
-
-        if (!ini_get("auto_detect_line_endings")) {
-            ini_set("auto_detect_line_endings", '1');
+        if (!ini_get('auto_detect_line_endings')) {
+            ini_set('auto_detect_line_endings', '1');
         }
 
         $famousPeople = Reader::createFromPath(resources_path('quotes/famous-people.txt'));
@@ -46,6 +40,5 @@ class QuoteController extends Controller
         $resToArray = iterator_to_array($res, false);
 
         return $resToArray[array_rand($resToArray, 1)]['name'];
-
     }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,5 @@
 
 function resources_path($path = '')
 {
-
-    return base_path('/resources/assets' . ($path ? DIRECTORY_SEPARATOR.$path : $path));
-
+    return base_path('/resources/assets'.($path ? DIRECTORY_SEPARATOR.$path : $path));
 }
