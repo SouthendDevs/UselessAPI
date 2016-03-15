e7a5587b48c9ae40835153323aa71421aac9ffd0
mikeaag <mike.gatward@xenonsoft.co.uk>
2016-03-15 18:13:11 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\File;
 use League\Csv\Reader;
 
 class QuoteController extends Controller
@@ -20,32 +17,28 @@ class QuoteController extends Controller
      */
     public function index(Request $request)
     {
-
         return Response::json([
-            'quote' => self::QUOTE_CHAR_L . 
-                       htmlentities($request->input('text'), ENT_QUOTES, 'UTF-8', false) . 
-                       self::QUOTE_CHAR_R . 
-                       self::DASH_CHAR . 
-                       $this->randomAttribution()]);
-
+            'quote' => self::QUOTE_CHAR_L.
+                       htmlentities($request->input('text'), ENT_QUOTES, 'UTF-8', false).
+                       self::QUOTE_CHAR_R.
+                       self::DASH_CHAR.
+                       $this->randomAttribution(), ]);
     }
 
     /**
-     * @return String
+     * @return string
      */
     public function randomAttribution()
     {
-
-        if (!ini_get("auto_detect_line_endings")) {
-            ini_set("auto_detect_line_endings", '1');
+        if (!ini_get('auto_detect_line_endings')) {
+            ini_set('auto_detect_line_endings', '1');
         }
 
-        $famousPeople = Reader::createFromPath(app_path() . '/Http/assets/famous-people.txt');
+        $famousPeople = Reader::createFromPath(app_path().'/Http/assets/famous-people.txt');
 
         $res = $famousPeople->fetchAssoc(['code', 'name']);
         $resToArray = iterator_to_array($res, false);
 
         return $resToArray[array_rand($resToArray, 1)]['name'];
-
     }
 }
--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -1,13 +1,9 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
 use App\Http\Controllers\CounterController;
 
 class CounterTest extends TestCase
-{   
+{
     public function testCounter()
     {
         CounterController::reset();
