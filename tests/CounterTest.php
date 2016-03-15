dbdec0eb830d177f2712b0b70173b0981c9317f0
mikeaag <mike.gatward@xenonsoft.co.uk>
2016-03-15 17:33:17 -0400

    Applied fixes from StyleCI

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CounterTest extends TestCase
 {
@@ -10,9 +7,9 @@ class CounterTest extends TestCase
     {
         Cache::store('file')->forever('counter_value', 10);
     }
-    
+
     public function testGetValue()
     {
-        echo "Cache value is: " . Cache::store('file')->get('counter_value') . "\n";
+        echo 'Cache value is: '.Cache::store('file')->get('counter_value')."\n";
     }
 }
